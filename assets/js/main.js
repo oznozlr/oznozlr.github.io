15a6b3f117a82c59450af2d3257d5f439de8dbcd
oznozlr <oznozlr@gmail.com>
2023-07-19 02:28:09 +0300

    yeni-site-yüklendi.

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*  --- asidenav  ----  */
+
+function openNav(){
+    document.getElementById("mySidenav").style.width ="350px";
+    
+}
+
+function closeNav(){
+    document.getElementById("mySidenav").style.width ="0";
+}
