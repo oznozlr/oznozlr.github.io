e152f2f56e6bbcc43925c6c8092aad4a321cbd7e
oznozlr <oznozlr@gmail.com>
2023-07-21 14:06:21 +0300

    mouse-yenilendi.

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,32 @@ function openNav(){
 function closeNav(){
     document.getElementById("mySidenav").style.width ="0";
 }
+
+
+/*  ---- mouse-style -----  */
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+    let mousePosX = 0,
+        mousePosY = 0,
+        mouseCircle = document.getElementById("mouse-circle");
+
+    document.onmousemove = (e) => {
+        mousePosX = e.pageX;
+        mousePosY = e.pageY;
+    };
+
+    let delay = 6,
+        revisedMousePosX = 0,
+        revisedMousePosY = 0;
+
+    function delayMouseFollow() {
+        requestAnimationFrame(delayMouseFollow);
+
+        revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+        revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+
+        mouseCircle.style.top = revisedMousePosY + "px";
+        mouseCircle.style.left = revisedMousePosX + "px";
+    }
+    delayMouseFollow();
+}
